6ce73265de0c3e4723ecbe61e6e72a4891cf4088
Jewel1311 <jewelmathew13@gmail.com>
2022-10-29 15:42:35 +0530

    updates

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ let el2 = document.getElementById("replay")
 el2.style.visibility = 'hidden' 
 
 function one() {
-    logic(1)
+        logic(1)
     }
 
 function two() {
@@ -19,45 +19,44 @@ function logic(num) {
     document.getElementById("ballcounter").innerHTML = ball 
     let el = document.getElementById("pick")
     el.style.visibility = 'hidden' 
-    computer(ball)
+
+    if(ball === 1){
+        lastball()
+        document.getElementById("msg1").className +=" text-center text-success "
+        document.getElementById("msg1").innerHTML = "You won "  
+    }
+    else{
+        computer(ball)
+    }
+}
+
+function lastball(){
+    let el1 = document.getElementById("msg1")
+    el1.style.visibility = 'visible' 
+    let el = document.getElementById("pick")
+    el.style.visibility = 'hidden' 
+    let el2 = document.getElementById("replay")
+    el2.style.visibility = 'visible'
 }
 
 function computer(b){
     setTimeout(function() {
-
-        if (b == 1){
-            let el1 = document.getElementById("msg1")
-            el1.style.visibility = 'visible' 
-            document.getElementById("msg1").innerHTML = "You won " 
-            let el = document.getElementById("pick")
-            el.style.visibility = 'hidden' 
-            let el2 = document.getElementById("replay")
-            el2.style.visibility = 'visible'
-            return
-        }
-
-        else if(b == 19 || b == 16 || b == 13 || b == 10 || b == 7 || b == 17 || b == 14 || b == 11 || b == 8
-            || b == 5 || b == 2 || b == 4 ) {
-                ball = ball - 1
-                document.getElementById("ballcounter").innerHTML = ball
-                document.getElementById("msg").innerHTML = "Computer took 1 ball"
-            }
-
-        else if(b == 18 || b == 15 || b == 12 || b == 9 || b == 6 || b == 3){
+        if(b%3 === 0){
             ball = ball - 2
             document.getElementById("ballcounter").innerHTML = ball
             document.getElementById("msg").innerHTML = "Computer took 2 balls"
         }
+        else {
+            ball = ball - 1
+            document.getElementById("ballcounter").innerHTML = ball
+            document.getElementById("msg").innerHTML = "Computer took 1 ball"
+        }
         
-        if (ball == 1){
-            let el1 = document.getElementById("msg1")
-            el1.style.visibility = 'visible' 
-            document.getElementById("msg1").style.backgroundColor = "red";
+        
+        if (ball == 1){ 
+            lastball()
+            document.getElementById("msg1").className += " text-center text-danger "
             document.getElementById("msg1").innerHTML = "You lost" 
-            let el = document.getElementById("pick")
-            el.style.visibility = 'hidden' 
-            let el2 = document.getElementById("replay")
-            el2.style.visibility = 'visible'
             return
         }
 
